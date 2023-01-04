607eec1510334748008d2dea743c60c91b4b3ef0
cuijiankang <jkcui@homingplus.com>
2023-01-04 19:23:35 +0800

    start mountComponent

--- vue3+/src/runtime/component.js
+++ vue3+/src/runtime/component.js
@@ -2,4 +2,21 @@ import { reactive, effect } from '../reactivity/index.js';
 import { queueJob } from './scheduler.js';
 import { normalizeVNode } from './vnode.js';
 
-export function mountComponent(vnode, container, anchor, patch) {}
+export function mountComponent(vnode, container, anchor, patch) {
+  const { type: Component } = vnode;
+  const instance = (vnode.component = {
+    props: null,
+    attrs: null,
+    setupState: null,
+    ctx: null,
+    mount: null,
+    update: null,
+    subTree: null,
+    isMounted: false,
+    next: null, // 储存n2
+  });
+
+  updateProps(instance, vnode);
+}
+
+function updateProps(instance, vnode) {}
